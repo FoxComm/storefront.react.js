0e623d7912608ad5e5f150eb63984d006c95a96a
Andrey Rublev <version.ru@gmail.com>
2017-06-20 14:16:52 +0200

    add checking for unique basename for styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,15 +2,25 @@
 const path = require('path');
 const fs = require('fs-extra');
 
+const seenPaths = Object.create(null);
+
 // '../../src/components/select/select' + 'option' -> 'fc/select__option'
 function generateScopedName(exportedName, filepath) {
-  const sanitisedPath = path.relative(process.cwd(), filepath)
+  const filepathRelative = path.relative(process.cwd(), filepath);
+  const sanitisedPath = filepathRelative
     .replace('src/components', '')
     .replace('src/css', '')
     .replace(/\.[^\.\/\\]+$/, '')
     .replace(/^[\.\/\\]+/, '');
 
   const finalPath = path.basename(sanitisedPath).replace(/\//g, '⁄');
+  if (finalPath in seenPaths) {
+    if (seenPaths[finalPath] != filepathRelative) {
+      throw new Error(`Couldn't use ${filepathRelative} in order to there is already file with same basename: ${seenPaths[finalPath]}`);
+    }
+  } else {
+    seenPaths[finalPath] = filepathRelative;
+  }
   const sanitisedName = exportedName.replace(/^_+|_+$/g, '');
 
   return `fc_${finalPath}__${sanitisedName}`;
