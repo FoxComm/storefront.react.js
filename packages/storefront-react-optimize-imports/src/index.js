12280b3203c37f084af7e22fa814ca0c7d3b9d40
Andrey Rublev <version.ru@gmail.com>
2017-06-24 14:21:52 +0200

    import import declarations directly from original source

--- packages/storefront-react-optimize-imports/src/index.js
+++ packages/storefront-react-optimize-imports/src/index.js
@@ -1,64 +1,62 @@
 'use strict';
-
 /* eslint new-cap: 0, dot-notation: 0, no-param-reassign: 0 */
 
 exports.__esModule = true;
 
-const corePackage = [
-  'Accordion',
-  'ActionLink',
-  ['ErrorAlerts', 'alerts/error-alerts'],
-  'Autocomplete',
-  ['Button', 'buttons/button'],
-  ['SecondaryButton', 'buttons/secondary-button'],
-  'Checkbox',
-  'Currency',
-  'EditableBlock',
-  'Facets',
-  'Filters',
-  ['Form', 'forms/form'],
-  ['FormField', 'forms/formfield'],
-  ['Counter', 'forms/counter'],
-  'Gallery',
-  ['GoogleConversion', 'google/conversion'],
-  'Icon',
-  'Modal',
-  'Overlay',
-  ['RadioButton', 'radiobutton/radiobutton'],
-  'Select',
-  ['SelectBox', 'selectbox/selectbox'],
-  'TermValueLine',
-  'TextInput',
-  ['TextArea', 'textarea/textarea'],
-  'WaitAnimation',
-  'WrapToLines',
-];
-
-const baseComponents = {
-  'ImagePlaceholder': 'product-image/image-placeholder',
-  'OrderTotals': 'order-summary/totals',
-  'OrderLineItems': 'order-summary/product-table',
-};
-
-function kebabCase(str) {
-  return str.replace(/([A-Z])/g, (a, m, i) => `-${m.toLowerCase()}`).substr(1);
-}
-
-const coreIndexed = corePackage.reduce((acc, value) => {
-  if (typeof value == 'string') {
-    const kebabName = kebabCase(value);
-    acc[value] = `core/${kebabName}/${kebabName}`;
-  } else {
-    acc[value[0]] = `core/${value[1]}`;
-  }
-  return acc;
-}, {});
-const components = Object.assign({}, baseComponents, coreIndexed);
+const { parse } = require('babylon');
+const path = require('path');
+const fs = require('fs');
+const findUp = require('find-up');
 
 const pkgName = '@foxcomm/storefront-react';
 
+const parentPackagePath = findUp.sync('package.json', {
+  cwd: path.resolve(__dirname, '../../')
+});
+
+if (!parentPackagePath) {
+  throw new Error('Could\'nt find parent root package');
+}
+let importsEntrypoint;
+
+const rootPackage = require(parentPackagePath);
+if (rootPackage.name === pkgName) {
+  importsEntrypoint = path.join(path.dirname(parentPackagePath), 'src/index.js');
+} else {
+  importsEntrypoint = path.join(path.dirname(parentPackagePath), 'node_modules', pkgName, 'src/index.js');
+}
+
+const importsCode = fs.readFileSync(importsEntrypoint).toString();
+const importsAst = parse(importsCode, {
+  sourceType: 'module',
+  plugins: [
+    'exportExtensions'
+  ]
+});
+
+function transformToComponents(importsAst, traverse) {
+  const components = Object.create(null);
+  traverse(importsAst, {
+    ExportNamedDeclaration(path) {
+      const { node } = path;
+      node.specifiers.forEach(spec => {
+        let entry = {
+          isDefault: spec.type === 'ExportDefaultSpecifier',
+          path: node.source.value.substr(1),
+        };
+        if (!entry.isDefault) {
+          entry.localName = spec.local.name;
+        }
+        components[spec.exported.name] = entry;
+      });
+    }
+  });
+  return components;
+}
+
 exports['default'] = function (_ref) {
   const t = _ref.types;
+  const components = transformToComponents(importsAst, _ref.traverse);
 
   return {
     visitor: {
@@ -67,15 +65,22 @@ exports['default'] = function (_ref) {
         if (node.source.value == pkgName) {
           const newImports = node.specifiers.map(decl => {
             const name = decl.imported.name;
-            const kebabName = kebabCase(name);
-
-            const importPath = components[name] || `${kebabName}/${kebabName}`;
-            const finalPath = `${pkgName}/lib/components/${importPath}`;
-
-            const source = t.stringLiteral(finalPath);
-            const specifier = t.importDefaultSpecifier(t.identifier(decl.local.name));
-
-            return t.importDeclaration([specifier], source);
+            if (name in components) {
+              const entry = components[name];
+              const source = t.stringLiteral(`${pkgName}/lib${entry.path}`);
+              let specifier;
+              if (entry.isDefault) {
+                specifier = t.importDefaultSpecifier(t.identifier(decl.local.name));
+              } else {
+                specifier = t.importSpecifier(t.identifier(decl.local.name), t.identifier(entry.localName));
+              }
+              return t.importDeclaration([specifier], source);
+            } else {
+              // unknown declaration, re-import as is
+              return t.importDeclaration([
+                t.importSpecifier(t.identifier(decl.local.name), t.identifier(decl.imported.name))
+              ], t.stringLiteral(node.source.value));
+            }
           });
           path.replaceWithMultiple(newImports);
         }
