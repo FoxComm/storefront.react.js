8832c0e81f85553f8d9529bc2ae6f53f2a17123d
Andrey Rublev <version.ru@gmail.com>
2017-06-28 11:55:12 +0200

    fmt

--- src/components/core/theme-provider/theme-provider.jsx
+++ src/components/core/theme-provider/theme-provider.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes, Component } from 'react';
 
 import * as themeData from './theme-data';
 
-type ContextType = {[key: string]: any};
+type ContextType = { [key: string]: any };
 
 type Props = {
   context?: ContextType,
